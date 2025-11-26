014b4f326b2a857e7d2ddc0bf4a40aa513e5209f
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2025-11-26 07:09:26 +0530

    Update default START_IMG_URL in config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/56cxwu.webm"
+    "START_IMG_URL", "https://files.catbox.moe/k7rs6w.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/k7rs6w.jpg"
